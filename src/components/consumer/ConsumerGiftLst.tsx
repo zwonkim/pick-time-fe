b64b94389cec77bc73abcc6dffb4d62cf55a9aeb
yooveloper <yooveloper@gmail.com>
2023-06-24 18:32:39 +0900

    feat: api 폴더 추가

---
+++ src/components/consumer/ConsumerGiftLst.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ConsumerGiftLst() {
+  return <h1>ConsumerGiftLst</h1>;
+}
---
+++ src/components/provider/ProviderGiftList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProviderGiftList() {
+  return <h1>ProviderGiftList</h1>;
+}
