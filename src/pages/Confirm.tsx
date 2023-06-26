5d658a7ef097b2f4e61d319a7f45fab36c9bd60e
Chaejung Kim <whenucan35@gmail.com>
2023-06-26 19:13:40 +0900

    fix: List 관련 mock data 추가 (#66)

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { GETGiftListResponse, getGiftList, useGETGiftList } from "api/api";
 import Button from "components/common/Button";
@@ -12,7 +13,7 @@ import styled from "styled-components";
 import { GiftList } from "types/giftList.type";
 // import { CouponList } from "types/couponList.type";
 
-const IS_MOCK = false;
+const IS_MOCK = true;
 
 // coupon 보류
 // export interface GiftCouponList {
@@ -36,13 +37,33 @@ function Confirm() {
     giftList: [
       {
         giftId: 123,
-        giftTitle: "CK 캘빈클라인 비 EDT 100ml",
         giftUrl:
-          "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000148683&chlNo=1&chlDtlNo=61&utm_source=naver&utm_medium=shopping_ep&utm_campaign=onpro_ep_product_0101_1231&utm_content=pc_price&nv_ad=pla&n_media=11068&n_query=%ED%96%A5%EC%88%98&n_rank=1&n_ad_group=grp-a001-02-000000013179677&n_ad=nad-a001-02-000000226442586&n_campaign_type=2&n_mall_id=oliveyoungshop&n_mall_pid=A000000148683&n_ad_group_type=2&NaPm=ct%3Dljbghxoo%7Cci%3D0zi0003bdDvyINl17f0o%7Ctr%3Dpla%7Chk%3Dacc0c9fff7e60837c1bbd4703c5516d5e19bb931",
+          "https://search.shopping.naver.com/catalog/9189323907?query=%ED%96%A5%EC%88%98&NaPm=ct%3Dljbsygag%7Cci%3D1fa4a4c46b1eec228e4b92fc25ad5128ba3d7243%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3D8739ded87f2e690b865acefc77f7f918d90be564",
         giftImage:
-          "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014868303ko.jpg?l=ko",
+          "https://shopping-phinf.pstatic.net/main_9189323/9189323907.20210408173258.jpg?type=f640",
+        giftTitle: "바이레도 블랑쉬 오 드 퍼퓸",
         giftDescription:
-          "본 상품 정보(상품 상세, 상품 설명 등)의 내용은 협력사가 직접 등록한 것 입니다.",
+          "종류 : 오 드 퍼퓸메인향 : 플로럴, 머스크사용대상 : 여성용탑노트 : 화이트로즈향, 핑크페퍼향, 알데히드향미들노트 : 바이올렛향, 네롤리향, 작약향베이스노트 : 블론드우드향, 샌들우드향, 머스크향",
+      },
+      {
+        giftId: 124,
+        giftUrl:
+          "https://search.shopping.naver.com/catalog/2600032275?query=%ED%96%A5%EC%88%98&NaPm=ct%3Dljbu3y48%7Cci%3Ddf2f47626b249c854d63d89feba7de4ca07fbb23%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3De0bcf8713741330056fc0b0340169f2635eb5e20",
+        giftImage:
+          "https://shopping-phinf.pstatic.net/main_2600032/2600032275.20210527163344.jpg?type=f640",
+        giftTitle: "샤넬 코코 마드모아젤 우먼 오 드 퍼퓸",
+        giftDescription:
+          "종류 : 오 드 퍼퓸메인향 : 플로럴, 오리엔탈사용대상 : 여성용탑노트 : 자스민향, 로즈향미들노트 : 플로랄향베이스노트 : 오리엔탈향",
+      },
+      {
+        giftId: 125,
+        giftUrl:
+          "https://search.shopping.naver.com/catalog/27614280524?query=%ED%96%A5%EC%88%98&NaPm=ct%3Dljbv4ta0%7Cci%3D969511a74e04425335c0264135ced00008b8dc98%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3Daab8a34ab0d5162e2f86fd619f20c2fc32039d1c",
+        giftImage:
+          "https://shopping-phinf.pstatic.net/main_2761428/27614280524.20210617155322.jpg?type=f640",
+        giftTitle: "메종마르지엘라 레이지 선데이 모닝 오 드 뚜왈렛",
+        giftDescription:
+          "종류 : 오 드 뚜왈렛메인향 : 플로럴머스크타입 : 스프레이사용대상 : 남녀공용주요제품특징 : 향탑노트 : 릴리오브더밸리향미들노트 : 아이리스향베이스노트 : 화이트머스크향, 사향씨향",
       },
     ],
     // couponList: [
@@ -73,29 +94,42 @@ function Confirm() {
     id: parseInt(targetId || "", 10),
   });
 
+  useEffect(() => {
+    if (IS_MOCK) {
+      fetchMockData();
+    }
+  }, [IS_MOCK]);
+
   return (
     <PageWrapper>
       <Header />
-      {!dataIsLoading && data && (
-        <>
-          <TitleWrapper>
-            <Title level={1} align="left">
-              <TitleSpan>{data?.providerName}님,</TitleSpan>
-              <br />
-              고르신 선물 확인해 주세요!
-            </Title>
-          </TitleWrapper>
-          <ListComponent listData={data?.giftList} type="default" />
-          <ButtonWrapper>
-            <Button
-              text="확인했어요!"
-              color={COLOR.PINK}
-              width="full"
-              onClick={onClickConfirmButton}
-            />
-          </ButtonWrapper>
-        </>
-      )}
+
+      <TitleWrapper>
+        <Title level={1} align="left">
+          <TitleSpan>
+            {IS_MOCK
+              ? userInfo?.providerName
+              : data
+              ? data.providerName
+              : "익명"}
+            님,
+          </TitleSpan>
+          <br />
+          고르신 선물 확인해 주세요!
+        </Title>
+      </TitleWrapper>
+      <ListComponent
+        listData={IS_MOCK ? fetchedList : data ? data?.giftList : []}
+        type="default"
+      />
+      <ButtonWrapper>
+        <Button
+          text="확인했어요!"
+          color={COLOR.PINK}
+          width="full"
+          onClick={onClickConfirmButton}
+        />
+      </ButtonWrapper>
     </PageWrapper>
   );
 }
--- src/pages/GiftForConsumer.tsx
+++ src/pages/GiftForConsumer.tsx
@@ -15,7 +15,7 @@ import COLOR from "style/color";
 import styled from "styled-components";
 import { GiftList } from "types/giftList.type";
 
-const IS_MOCK = false;
+const IS_MOCK = true;
 
 type User = Pick<GETGiftListResponse, "providerName" | "consumerName">;
 
@@ -35,63 +35,33 @@ function GiftForConsumer() {
     giftList: [
       {
         giftId: 123,
-        giftTitle: "CK 캘빈클라인 비 EDT 100ml",
         giftUrl:
-          "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000148683&chlNo=1&chlDtlNo=61&utm_source=naver&utm_medium=shopping_ep&utm_campaign=onpro_ep_product_0101_1231&utm_content=pc_price&nv_ad=pla&n_media=11068&n_query=%ED%96%A5%EC%88%98&n_rank=1&n_ad_group=grp-a001-02-000000013179677&n_ad=nad-a001-02-000000226442586&n_campaign_type=2&n_mall_id=oliveyoungshop&n_mall_pid=A000000148683&n_ad_group_type=2&NaPm=ct%3Dljbghxoo%7Cci%3D0zi0003bdDvyINl17f0o%7Ctr%3Dpla%7Chk%3Dacc0c9fff7e60837c1bbd4703c5516d5e19bb931",
+          "https://search.shopping.naver.com/catalog/9189323907?query=%ED%96%A5%EC%88%98&NaPm=ct%3Dljbsygag%7Cci%3D1fa4a4c46b1eec228e4b92fc25ad5128ba3d7243%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3D8739ded87f2e690b865acefc77f7f918d90be564",
         giftImage:
-          "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014868303ko.jpg?l=ko",
+          "https://shopping-phinf.pstatic.net/main_9189323/9189323907.20210408173258.jpg?type=f640",
+        giftTitle: "바이레도 블랑쉬 오 드 퍼퓸",
         giftDescription:
-          "본 상품 정보(상품 상세, 상품 설명 등)의 내용은 협력사가 직접 등록한 것 입니다.",
+          "종류 : 오 드 퍼퓸메인향 : 플로럴, 머스크사용대상 : 여성용탑노트 : 화이트로즈향, 핑크페퍼향, 알데히드향미들노트 : 바이올렛향, 네롤리향, 작약향베이스노트 : 블론드우드향, 샌들우드향, 머스크향",
       },
       {
-        giftId: 1234,
-        giftTitle: "CK 캘빈클라인 비 EDT 100ml",
+        giftId: 124,
         giftUrl:
-          "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000148683&chlNo=1&chlDtlNo=61&utm_source=naver&utm_medium=shopping_ep&utm_campaign=onpro_ep_product_0101_1231&utm_content=pc_price&nv_ad=pla&n_media=11068&n_query=%ED%96%A5%EC%88%98&n_rank=1&n_ad_group=grp-a001-02-000000013179677&n_ad=nad-a001-02-000000226442586&n_campaign_type=2&n_mall_id=oliveyoungshop&n_mall_pid=A000000148683&n_ad_group_type=2&NaPm=ct%3Dljbghxoo%7Cci%3D0zi0003bdDvyINl17f0o%7Ctr%3Dpla%7Chk%3Dacc0c9fff7e60837c1bbd4703c5516d5e19bb931",
+          "https://search.shopping.naver.com/catalog/2600032275?query=%ED%96%A5%EC%88%98&NaPm=ct%3Dljbu3y48%7Cci%3Ddf2f47626b249c854d63d89feba7de4ca07fbb23%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3De0bcf8713741330056fc0b0340169f2635eb5e20",
         giftImage:
-          "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014868303ko.jpg?l=ko",
+          "https://shopping-phinf.pstatic.net/main_2600032/2600032275.20210527163344.jpg?type=f640",
+        giftTitle: "샤넬 코코 마드모아젤 우먼 오 드 퍼퓸",
         giftDescription:
-          "본 상품 정보(상품 상세, 상품 설명 등)의 내용은 협력사가 직접 등록한 것 입니다.",
+          "종류 : 오 드 퍼퓸메인향 : 플로럴, 오리엔탈사용대상 : 여성용탑노트 : 자스민향, 로즈향미들노트 : 플로랄향베이스노트 : 오리엔탈향",
       },
       {
-        giftId: 12345,
-        giftTitle: "CK 캘빈클라인 비 EDT 100ml",
+        giftId: 125,
         giftUrl:
-          "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000148683&chlNo=1&chlDtlNo=61&utm_source=naver&utm_medium=shopping_ep&utm_campaign=onpro_ep_product_0101_1231&utm_content=pc_price&nv_ad=pla&n_media=11068&n_query=%ED%96%A5%EC%88%98&n_rank=1&n_ad_group=grp-a001-02-000000013179677&n_ad=nad-a001-02-000000226442586&n_campaign_type=2&n_mall_id=oliveyoungshop&n_mall_pid=A000000148683&n_ad_group_type=2&NaPm=ct%3Dljbghxoo%7Cci%3D0zi0003bdDvyINl17f0o%7Ctr%3Dpla%7Chk%3Dacc0c9fff7e60837c1bbd4703c5516d5e19bb931",
+          "https://search.shopping.naver.com/catalog/27614280524?query=%ED%96%A5%EC%88%98&NaPm=ct%3Dljbv4ta0%7Cci%3D969511a74e04425335c0264135ced00008b8dc98%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3Daab8a34ab0d5162e2f86fd619f20c2fc32039d1c",
         giftImage:
-          "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014868303ko.jpg?l=ko",
+          "https://shopping-phinf.pstatic.net/main_2761428/27614280524.20210617155322.jpg?type=f640",
+        giftTitle: "메종마르지엘라 레이지 선데이 모닝 오 드 뚜왈렛",
         giftDescription:
-          "본 상품 정보(상품 상세, 상품 설명 등)의 내용은 협력사가 직접 등록한 것 입니다.",
-      },
-      {
-        giftId: 12345778,
-        giftTitle: "CK 캘빈클라인 비 EDT 100ml",
-        giftUrl:
-          "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000148683&chlNo=1&chlDtlNo=61&utm_source=naver&utm_medium=shopping_ep&utm_campaign=onpro_ep_product_0101_1231&utm_content=pc_price&nv_ad=pla&n_media=11068&n_query=%ED%96%A5%EC%88%98&n_rank=1&n_ad_group=grp-a001-02-000000013179677&n_ad=nad-a001-02-000000226442586&n_campaign_type=2&n_mall_id=oliveyoungshop&n_mall_pid=A000000148683&n_ad_group_type=2&NaPm=ct%3Dljbghxoo%7Cci%3D0zi0003bdDvyINl17f0o%7Ctr%3Dpla%7Chk%3Dacc0c9fff7e60837c1bbd4703c5516d5e19bb931",
-        giftImage:
-          "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014868303ko.jpg?l=ko",
-        giftDescription:
-          "본 상품 정보(상품 상세, 상품 설명 등)의 내용은 협력사가 직접 등록한 것 입니다.",
-      },
-      {
-        giftId: 123456,
-        giftTitle: "CK 캘빈클라인 비 EDT 100ml",
-        giftUrl:
-          "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000148683&chlNo=1&chlDtlNo=61&utm_source=naver&utm_medium=shopping_ep&utm_campaign=onpro_ep_product_0101_1231&utm_content=pc_price&nv_ad=pla&n_media=11068&n_query=%ED%96%A5%EC%88%98&n_rank=1&n_ad_group=grp-a001-02-000000013179677&n_ad=nad-a001-02-000000226442586&n_campaign_type=2&n_mall_id=oliveyoungshop&n_mall_pid=A000000148683&n_ad_group_type=2&NaPm=ct%3Dljbghxoo%7Cci%3D0zi0003bdDvyINl17f0o%7Ctr%3Dpla%7Chk%3Dacc0c9fff7e60837c1bbd4703c5516d5e19bb931",
-        giftImage:
-          "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014868303ko.jpg?l=ko",
-        giftDescription:
-          "본 상품 정보(상품 상세, 상품 설명 등)의 내용은 협력사가 직접 등록한 것 입니다.",
-      },
-      {
-        giftId: 123457,
-        giftTitle: "CK 캘빈클라인 비 EDT 100ml",
-        giftUrl:
-          "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000148683&chlNo=1&chlDtlNo=61&utm_source=naver&utm_medium=shopping_ep&utm_campaign=onpro_ep_product_0101_1231&utm_content=pc_price&nv_ad=pla&n_media=11068&n_query=%ED%96%A5%EC%88%98&n_rank=1&n_ad_group=grp-a001-02-000000013179677&n_ad=nad-a001-02-000000226442586&n_campaign_type=2&n_mall_id=oliveyoungshop&n_mall_pid=A000000148683&n_ad_group_type=2&NaPm=ct%3Dljbghxoo%7Cci%3D0zi0003bdDvyINl17f0o%7Ctr%3Dpla%7Chk%3Dacc0c9fff7e60837c1bbd4703c5516d5e19bb931",
-        giftImage:
-          "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014868303ko.jpg?l=ko",
-        giftDescription:
-          "본 상품 정보(상품 상세, 상품 설명 등)의 내용은 협력사가 직접 등록한 것 입니다.",
+          "종류 : 오 드 뚜왈렛메인향 : 플로럴머스크타입 : 스프레이사용대상 : 남녀공용주요제품특징 : 향탑노트 : 릴리오브더밸리향미들노트 : 아이리스향베이스노트 : 화이트머스크향, 사향씨향",
       },
     ],
   };
@@ -125,6 +95,12 @@ function GiftForConsumer() {
   } = useGETGiftList({
     id: parseInt(targetId || "", 10),
   });
+
+  useEffect(() => {
+    if (IS_MOCK) {
+      fetchMockData();
+    }
+  }, [IS_MOCK]);
 
   return (
     <PageWrapper>
