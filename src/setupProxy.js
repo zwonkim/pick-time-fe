5d9fb9cbdeb7a3b84ecfcc7be735f618b94d54a9
Chaejung Kim <whenucan35@gmail.com>
2023-07-21 15:56:06 +0900

    fix: proxy 설정 (#83)
    - 모든 api uri는 `/api/`로 시작합니다
    - 해당 설정은 CORS 에러 방지를 위함입니다

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,31 +3,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://15.164.225.241", // 실제 API 엔드포인트의 주소로 대체해야 합니다.
+    createProxyMiddleware("/api/**", {
+      target: "https://api.picktime.store", // 실제 API 엔드포인트의 주소로 대체해야 합니다.
       changeOrigin: true,
       pathRewrite: {
         "^/api": "", // 프록시 경로에서 '/api'를 제거합니다.
       },
     }),
   );
-  // 이거다!!
-  // app.use(
-  //   "/scrape",
-  //   createProxyMiddleware({
-  //     target: "http://localhost:5050",
-  //     changeOrigin: true,
-  //   }),
-  // );
-  //
-  // 배포 세팅
-  app.use(
-    "/scrape",
-    createProxyMiddleware({
-      target: "https://political-olive-radio.glitch.me",
-      changeOrigin: true,
-    }),
-  );
-  //
 };
